f5afe5ab246a5e401539fef3be7eb6c458935de2
Elieber Souza <elieber.souzans@gmail.com>
2017-07-18 23:40:20 -0400

    Criados graficos por mes e por tipo de violencia

--- static/scripts/ChartController.js
+++ static/scripts/ChartController.js
@@ -5,24 +5,43 @@
         .module('bobotApp')
         .controller('ChartController', ChartController);
 
-    ChartController.$inject = ['$scope', 'GoogleMapsService', 'Violence','$http', '$uibModal', '$sessionStorage'];
+    ChartController.$inject = ['$scope', 'DateUtils', 'Violence','$http', '$uibModal', '$sessionStorage', '$q', '_'];
 
-    function ChartController ($scope,GoogleMapsService,Violence, $http, $uibModal, $sessionStorage) {
+    function ChartController ($scope,DateUtils,Violence, $http, $uibModal, $sessionStorage, $q, _) {
         var vm = this;
-        vm.data = {
-            labels: ["January", "February", "March", "April", "May", "June", "July"],
-            datasets: [
-                {
-                    fillColor: "rgba(220,220,220,0.4)",
-                    strokeColor: "rgba(220,220,220,1)",
-                    data: [65, 59, 80, 81, 56, 55, 40]
-                },
-                {
-                    fillColor: "rgba(151,187,205,0.4)",
-                    strokeColor: "rgba(151,187,205,1)",
-                    data: [28, 48, 40, 19, 86, 27, 90]
+        vm.violencias = Violence.query();
+        $q.all([vm.violencias.$promise]).then(function () {
+            var groupedByMonth = _.groupBy(vm.violencias, function(item) {
+                if(item.violence_date) {
+                    return item.violence_date.substring(6, 7);
                 }
-            ]
+            });
+            var groupdByTipo = _.countBy(vm.violencias, 'violence_type');
+            vm.pie.labels = _.map(groupdByTipo, function (value, key) {
+                return key;
+            });
+            vm.pie.data = _.map(groupdByTipo, function (value, key) {
+                return value;
+            });
+            console.log(vm.pie.labels);
+            var dataMes = [];
+            delete groupedByMonth[undefined];
+            for(var i = 0; i < 12; i++){
+
+                dataMes[i] = (groupedByMonth[i+1] ? groupedByMonth[i+1].length : 0);
+            }
+            vm.bar.data.push(dataMes);
+            console.log(vm.bar.data);
+        });
+
+        vm.bar = {
+            labels: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'],
+            series: ['Series A'],
+            data:[]
+
+        };
+
+        vm.pie = {
         };
 }
 })();
