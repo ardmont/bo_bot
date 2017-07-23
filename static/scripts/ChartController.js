e2b4241322f65eb2024698e55f301f3020c331d7
Elieber Souza <elieber.souzans@gmail.com>
2017-07-23 10:59:49 -0400

    Implementados filtros nos graficos

--- static/scripts/ChartController.js
+++ static/scripts/ChartController.js
@@ -5,9 +5,9 @@
         .module('bobotApp')
         .controller('ChartController', ChartController);
 
-    ChartController.$inject = ['Violence','$q', '_'];
+    ChartController.$inject = ['Violence','$q', '_', '$scope'];
 
-    function ChartController (Violence, $q, _) {
+    function ChartController (Violence, $q, _, $scope) {
         var vm = this;
         vm.violencias = Violence.query();
         //Objeto que contem os dados do grafico pizza
@@ -21,6 +21,7 @@
         };
 
         //Compila os dados indos do webservice e agrupa para apresentar nos graficos
+
         $q.all([vm.violencias.$promise]).then(function () {
             var groupedByMonth = _.groupBy(vm.violencias, function(item) {
                 if(item.violence_date) {
@@ -45,7 +46,35 @@
             console.log(vm.bar.data);
         });
 
+        function compilaDadosFiltros(dados) {
+            vm.bar.data = [];
+            vm.pie.data =[];
+            var groupedByMonth = _.groupBy(dados, function(item) {
+                if(item.violence_date) {
+                    return item.violence_date.substring(6, 7);
+                }
+            });
+            var groupdByTipo = _.countBy(dados, 'violence_type');
+            vm.pie.labels = _.map(groupdByTipo, function (value, key) {
+                return key;
+            });
+            vm.pie.data = _.map(groupdByTipo, function (value, key) {
+                return value;
+            });
+            console.log(vm.pie.labels);
+            var dataMes = [];
+            delete groupedByMonth[undefined];
+            for(var i = 0; i < 12; i++){
 
+                dataMes[i] = (groupedByMonth[i+1] ? groupedByMonth[i+1].length : 0);
+            }
+            vm.bar.data.push(dataMes);
+            console.log(vm.bar.data);
+        }
+
+        $scope.$on('filtroSuccess', function (event, result) {
+            compilaDadosFiltros(result);
+        })
 
 
 }
